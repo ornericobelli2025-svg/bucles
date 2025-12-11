3373f3e00f577a52ef22972cd67dab1d909ad434
Ornella Ricobelli <ornericobelli2025@gmail.com>
2025-12-11 03:34:44 -0300

    add eje

---
+++ js/tp.js
@@ -0,0 +1,23 @@
+// 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+// Ejemplo: 
+// input: 20
+// Output: El 20 es divisible por 2.
+
+
+// const numero = parseInt(prompt('Ingrese un número:'))
+
+// if (numero % 2 == 0) {
+//     document.writeln (`el numero ${numero} es divisible por 2`)
+// }
+// else {
+//     document.writeln (`el numero ${numero} no es divisible por 2`)
+// }
+
+
+// 3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+// Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
+
+
+
+
